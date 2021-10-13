dbf5b8d7196e18c1316fae879bf47869b96041f2
Andrew Simmons <a.simmons@deakin.edu.au>
2021-10-13 20:39:52 +1100

    NEW: Scripts to call pylint with context specific configuration

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+
 # 1. TODO: pre-learn some things about the context.
 
 
@@ -51,7 +55,7 @@ def list_files():
 
 
 def is_ml_file(file_name):
-    print("-------------")
+    # print("-------------")
     file1 = open(file_name, "r")
     lines = file1.readlines()
 
@@ -59,13 +63,13 @@ def is_ml_file(file_name):
     # Strips the newline character.
     for line in lines:
         count += 1
-        print("Line{}: {}".format(count, line.strip()))
-        print("type(line):", type(line))
+        # print("Line{}: {}".format(count, line.strip()))
+        # print("type(line):", type(line))
         for ml_import in ml_imports:
             if ml_import in line.strip():
-                print("file_name:", file_name)
-                print("line_number:", count)
-                print("ml_import:", ml_import)
+                # print("file_name:", file_name)
+                # print("line_number:", count)
+                # print("ml_import:", ml_import)
                 return True
 
     # If no valid import was found, return False.
@@ -85,9 +89,7 @@ def run_pylint():
     print("run_pylint")
 
 
-# main execution.
-if __name__ == "__main__":
-    print("__main__")
+def classify_all():
     file_names = list_files()
     classification = {}
     for file_name in file_names:
@@ -95,3 +97,33 @@ if __name__ == "__main__":
 
     print("classification:")
     print(classification)
+
+
+# main execution.
+# Usage: python3 main.py example-src/ml.py context
+# Outputs: pylint configuration file to use
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        # Classify a single file
+        file_name = sys.argv[1]
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        ml_config_path = os.path.join(script_dir, "example-src/configs/ml.pylintrc")
+        non_ml_config_path = os.path.join(script_dir, "example-src/configs/non_ml.pylintrc")
+
+        if len(sys.argv) > 2:
+            mode = sys.argv[2]
+        else:
+            mode = "context"
+
+        if mode == "non_ml":
+            config_path = non_ml_config_path
+        elif mode == "ml":
+            config_path = ml_config_path
+        else:
+            # context aware
+            config_path = ml_config_path if is_ml_file(file_name) else non_ml_config_path
+
+        print(config_path)
+    else:
+        # If no file provided, classify all files
+        classify_all()
